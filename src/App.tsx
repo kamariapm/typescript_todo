3dacbef9aa2c72f5983efbaec35dbdb24956ceb7
kamariapm <kamariapm@gmail.com>
2022-08-29 23:21:32 -0400

    added todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { TodoForm } from "./components/TodoForm";
 
 function App() {
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+  const addTodo: AddTodo = (newTodo) => {
+    if (newTodo !== "") {
+      setTodos([...todos, { text: newTodo, complete: false }]);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo</h1>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
